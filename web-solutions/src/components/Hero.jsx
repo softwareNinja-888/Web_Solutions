0a17cb8d21e970b70baa1f682fd743d78d068313
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-04-05 20:03:13 +0200

    Make hero section responsive

--- web-solutions/src/components/Hero.jsx
+++ web-solutions/src/components/Hero.jsx
@@ -33,16 +33,16 @@ export function Hero(){
     
     return (
         <>
-            <motion.div variants={containerVariants} initial="hidden" animate="visible" className="flex flex-col md:flex-row  gap-16 w-screen not-[]:px-6 md:px-16 py-20">
+            <motion.div variants={containerVariants} initial="hidden" animate="visible" className="flex flex-col md:flex-row  gap-16 w-screen not-[]:px-6 md:px-16 py-20 ">
 
                 {/* TEXT-CONTENT */}
                 <motion.div variants={itemVariants} className="flex flex-col text-white self-center gap-8 flex-2/3">
-                    <GradientText text={`${companyName}: Accelerate Digital Success`} width="w-full" h="h-20" size="text-4xl"/>
-                    <div className="text-md font-poppins">We craft tailored solutions that connect your business with local audiences and global opportunities. Whether you're a startup or an established brand, let us turn clicks into customers and traffic into revenue.  
+                    <GradientText text={`${companyName}: Accelerate Digital Success`} width="w-full lg:text-start text-center" h="h-20" size="text-4xl"/>
+                    <div className="text-md font-poppins lg:text-start text-center">We craft tailored solutions that connect your business with local audiences and global opportunities. Whether you're a startup or an established brand, let us turn clicks into customers and traffic into revenue.  
                     </div>
 
                     {/* ADD ACTION AFTER CLICKING BUTTON */}
-                    <NavLink to='/contact' className="group flex justify-center items-center gap-3 border border-greenPrimary py-5 w-72 cursor-pointer hover:bg-greenPrimary hover:text-black transition duration-400 ease-in-out">
+                    <NavLink to='/contact' className="group flex justify-center items-center gap-3 border border-greenPrimary py-5 w-72 cursor-pointer hover:bg-greenPrimary hover:text-black transition duration-400 ease-in-out mx-auto lg:mx-0">
                         <div className="font-inter700">Get a Free Consultaion</div>
                         <FaArrowRight size={24}  className="text-greenPrimary group-hover:text-black"/>
                     </NavLink>
